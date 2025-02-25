b4e2e6b4464eef764542e43757cbbb9428d2ce1e
Aliyu Abubakar <aliyufari@gmail.com>
2025-02-25 22:50:18 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const { errorHandler } = require('./middlewares/errorMiddleware')
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 8080;
+
+app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
+
+app.listen(port, console.log(`Listening on ${port}`));
